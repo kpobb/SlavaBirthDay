862f880e9ec49134d94f5a3e40a53a612d0a0d37
Oleksandr Kushnir <oleksandr.kushnir@win-interactive.com>
2015-10-29 12:18:46 +0200

    Init

---
+++ SlavaBirthDay/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SlavaBirthDay
+{
+    class Program
+    {
+        private const bool HasGirlFriend = false;
+        private const bool HasOwnHouse = false;
+        private const bool HasGoodHealth = true;
+        private const bool HasFriends = true;
+        private const bool HasLifeGoals = true;
+        private const bool IsPositivePerson = true;
+
+        static void Main()
+        {
+            if (DateTime.Now.Date.Equals(new DateTime(2015, 10, 29)))
+            {
+                Console.Write("Привет имениннеГ. Хочу поздравить тебя с Днем Рождения! ");
+                Console.Write("Желаю тебе ");
+
+                if (HasGoodHealth)
+                {
+                    Console.Write("богатырского здоровья, ");
+                }
+
+                if (IsPositivePerson)
+                {
+                    Console.Write("море счастья, ");
+                }
+
+                if (!HasOwnHouse)
+                {
+                    Console.Write("мешок денег, ");
+                }
+
+                if (HasLifeGoals)
+                {
+                    Console.Write("и успехов во всех начинаниях. ");
+                }
+
+                if (!HasGirlFriend)
+                {
+                    Console.Write("Пусть тебя окружают прекрасные женщины, ");
+                }
+
+                if (HasFriends)
+                {
+                    Console.Write("верные товарищи и просто хорошие люди. ");
+                }
+
+                Console.WriteLine("С ДНЮХОЙ!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
